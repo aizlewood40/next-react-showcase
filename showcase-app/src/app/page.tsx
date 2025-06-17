a00efe6940c258bbb65ace6789c977effd22ac28
Matthew Aizlewood <matthew.aizlewood@flutteruki.com>
2025-06-17 19:39:37 +0100

    feat: implement patient creation

--- showcase-app/src/app/page.tsx
+++ showcase-app/src/app/page.tsx
@@ -1,12 +1,13 @@
 import styles from "./page.module.css";
 import TestComponent from "./components/TestComponent";
 import PatientsGrid from "./components/PatientsGrid";
+import NewPatientForm from "./components/NewPatientForm";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <PatientsGrid />
-      <TestComponent />
+      <NewPatientForm />
     </div>
   );
 }
